455570fee02389e7cdce7da3d82f312e69a31290
ayyan915 <bandhack12@gmail.com>
2026-09-15 13:57:41 +0500

    Add Procfile, update requirements for Render deploy

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from extension import app, socketio, games
 from sockets import handle_sockets
 from routes.game import game
@@ -5,12 +6,8 @@ from routes.game import game
 
 handle_sockets(socketio, games)
 app.register_blueprint(game)
+
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
-
-
-
-
-
-
-
+    port = int(os.getenv("PORT", 5000))
+    debug = os.getenv("FLASK_DEBUG", "false").lower() == "true"
+    socketio.run(app, host="0.0.0.0", port=port, debug=debug)
